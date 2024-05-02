5416670407d039dd45eeaf6db90420bc355d81a9
AistVolhv <istanik@ya.ru>
2024-05-02 10:08:27 +0500

    Добавление модулей

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import {NewFilterView} from './view/new-filter-view.js';
-import {render, renderAny} from './render.js';
+import NewFilterView from './view/new-filters.js';
+import TripEventsView from './view/new-trip-events.js';
+import TripEventsListView from './view/new-trip-events-list.js';
+
+
+import {render} from './render.js';
 
 const siteBody = document.querySelector('.page-body');
 const siteControlFilters = siteBody.querySelector('.trip-controls__filters');
+const siteControlTripEvents = siteBody.querySelector('.trip-events');
 
 render(new NewFilterView(), siteControlFilters);
-renderAny(new NewFilterView(), siteControlFilters);
+render(new TripEventsView(), siteControlTripEvents);
+render(new TripEventsListView(), siteControlTripEvents);
--- src/render.js
+++ src/render.js
@@ -8,33 +8,11 @@ const RenderPosition = {
 function createElement(template) {
   const newElement = document.createElement('div');
   newElement.innerHTML = template;
-  console.log(newElement.children);
-
   return newElement.firstElementChild;
 }
-/*
-function createFragment(template) {
-  const newElement = document.createElement('div');
-  newElement.innerHTML = template;
-  console.log(newElement.children);
-
-  return newElement.children;
-}
-*/
-const createFragment = (elements) => {
-  const fragment = document.createDocumentFragment;
-  elements.forEach((element) => {
-    fragment.appendChild(element);
-  });
-  return fragment;
-};
-
-const renderAny = (component, container) => {
-  container.append(component.getFragment());
-};
 
 function render(component, container, place = RenderPosition.BEFOREEND) {
   container.insertAdjacentElement(place, component.getElement());
 }
 
-export {RenderPosition, createElement, createFragment, render, renderAny,};
+export {RenderPosition, createElement, render};
