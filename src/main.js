7c3e708087b9c931635ffcc5e88110519b3788c4
AistVolhv <istanik@ya.ru>
2024-05-03 13:37:10 +0500

    Много правок.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,8 @@
-import NewFilterView from './view/new-filters.js';
-import TripEventsView from './view/new-trip-events.js';
-import TripEventsListView from './view/new-trip-events-list.js';
-
-
-import {render} from './render.js';
+import BoardPresenter from './presenter/board-presenter.js';
 
 const siteBody = document.querySelector('.page-body');
-const siteControlFilters = siteBody.querySelector('.trip-controls__filters');
-const siteControlTripEvents = siteBody.querySelector('.trip-events');
 
-render(new NewFilterView(), siteControlFilters);
-render(new TripEventsView(), siteControlTripEvents);
-render(new TripEventsListView(), siteControlTripEvents);
+const boardPresenter = new BoardPresenter({boardContainer: siteBody});
+
+boardPresenter.init();
+
---
+++ src/presenter/board-presenter.js
@@ -0,0 +1,44 @@
+import {RenderPosition, render} from '../render.js';
+
+import NewTripInfo from '../view/new-trip-info.js';
+import NewFilterView from '../view/new-filters.js';
+import NewListSortView from '../view/new-list-sort.js';
+import NewListEvents from '../view/new-list-events-view.js';
+import NewAddEventView from '../view/add-trip-event-view.js';
+import NewEditEventView from '../view/edit-trip-event-view.js';
+import NewListEventsPoint from '../view/list-events-point-view.js';
+
+//const siteBody = document.querySelector('.page-body');
+//const bodyHeaderTripMain = siteBody.querySelector('.trip-main');
+//const bodyHeaderTripMain = siteBody.querySelector('.trip-main');
+//const siteControlFilters = siteBody.querySelector('.trip-controls__filters');
+//const siteControlTripEvents = siteBody.querySelector('.trip-events');
+
+export default class BoardPresenter {
+
+  eventListComponent = new NewListEvents();
+
+  constructor ({boardContainer}) {
+    this.boardContainer = boardContainer;
+    this.bodyHeaderTripMain = this.boardContainer.querySelector('.trip-main');
+    this.siteControlFilters = this.boardContainer.querySelector('.trip-controls__filters');
+    this.siteControlTripEvents = this.boardContainer.querySelector('.trip-events');
+  }
+
+  init () {
+    render(new NewTripInfo(), this.bodyHeaderTripMain, RenderPosition.AFTERBEGIN);
+    render(new NewFilterView(), this.siteControlFilters);
+    render(new NewListSortView(), this.siteControlTripEvents);
+
+    render(this.eventListComponent, this.siteControlTripEvents);
+
+    render(new NewAddEventView(), this.eventListComponent.getElement());
+    render(new NewEditEventView(), this.eventListComponent.getElement());
+
+    for (let i = 0; i < 3; i++) {
+      render(new NewListEventsPoint(), this.eventListComponent.getElement());
+    }
+
+  }
+
+}
