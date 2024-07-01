50735c4751cf2dc4a05af477354c214dc3674b2f
AistVolhv <istanik@ya.ru>
2024-07-01 13:57:41 +0500

    Правка шаблона для сортировки

--- src/consts.js
+++ src/consts.js
@@ -1,3 +1,18 @@
-const COLORS = ['black', 'yellow', 'blue', 'green', 'pink'];
 
-export {COLORS};
+const FilterType = {
+  EVERYTHING: 'EVERYTHING',
+  FUTURE: 'FUTURE',
+  PRESENT: 'PRESENT',
+  PAST: 'PAST',
+};
+
+const SortType = {
+  DAY: 'DAY',
+  EVENT: 'EVENT',
+  TIME: 'TIME',
+  PRICE: 'PRICE',
+  OFFERS: 'OFFERS'};
+
+const sortMap = ['day', 'event', 'time', 'price', 'offers'];
+
+export {FilterType,SortType,sortMap};
--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,32 +1,28 @@
 import AbstractView from '../framework/view/abstract-view';
+import {SortType,sortMap} from '../consts.js';
+
+function createSortItemTemplate(sortType) {
+  //
+  let disable = '';
+
+  if (sortType === 'event' || sortType === 'offers') {
+    disable = 'disabled';
+   } else {
+    disable = '';
+   };
+
+  return (`
+  <div class="trip-sort__item  trip-sort__item--${sortType}">
+      <input id="sort-${sortType}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${sortType}" ${disable}>
+      <label class="trip-sort__btn" for="sort-day">${sortType}</label>
+    </div>
+  `);
+};
 
 function createSortTemplate() {
   return (`
   <form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-    <div class="trip-sort__item  trip-sort__item--day">
-      <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day">
-      <label class="trip-sort__btn" for="sort-day">Day</label>
-    </div>
-
-    <div class="trip-sort__item  trip-sort__item--event">
-      <input id="sort-event" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-event" disabled>
-      <label class="trip-sort__btn" for="sort-event">Event</label>
-    </div>
-
-    <div class="trip-sort__item  trip-sort__item--time">
-      <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
-      <label class="trip-sort__btn" for="sort-time">Time</label>
-    </div>
-
-    <div class="trip-sort__item  trip-sort__item--price">
-      <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" checked>
-      <label class="trip-sort__btn" for="sort-price">Price</label>
-    </div>
-
-    <div class="trip-sort__item  trip-sort__item--offer">
-      <input id="sort-offer" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-offer" disabled>
-      <label class="trip-sort__btn" for="sort-offer">Offers</label>
-    </div>
+  ${sortMap.map((item) => createSortItemTemplate(item)).join('')}
   </form>
 `);
 }
