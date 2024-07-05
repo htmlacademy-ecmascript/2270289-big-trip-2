aedd7ef918a3278dc3523c45f3519b3a496de735
AistVolhv <istanik@ya.ru>
2024-07-05 10:57:47 +0500

    Перемещение кода. Point-presenter

--- src/consts.js
+++ src/consts.js
@@ -17,5 +17,10 @@ const sortMap = ['day', 'event', 'time', 'price', 'offers'];
 const filterMap = ['everything','future','present','past'];
 const pathPointMap = ['Amsterdam', 'Geneva', 'Chamonix'];
 
+const Mode = {
+  DEFAULT: 'DEFAULT',
+  EDITING: 'EDITING',
+};
 
-export {FilterType,filterMap,SortType,sortMap,pathPointMap};
+
+export {FilterType,filterMap,SortType,sortMap,pathPointMap,Mode};
--- src/presenter/point-presenter.js
+++ src/presenter/point-presenter.js
@@ -1,27 +1,31 @@
-import {render,replace} from '../framework/render.js';
+import {render,replace,remove} from '../framework/render.js';
+
+import {Mode} from '../consts.js';
+
 
 import EditPointView from '../view/edit-point-view.js';
 import PointView from '../view/point-view.js';
 
 export default class PointPresenter {
-  //#points = null;
+  #point = null;
   #destinations = null;
   #offers = null;
   #placeRenderList =null;
-  //#placeRenderEmpty = null;
 
-  #point = null;
   #pointComponent = null;
   #editPointComponent = null;
+  #newPointComponent = null;
 
-  constructor ({point,destinations,offers,placeRenderList}) {
+  #mode = Mode.DEFAULT;
+
+  constructor ({point,destinations,offers,placeRenderList,onDataChange,onModeChange}) {
     this.#point = point;
     this.#destinations =destinations;
     this.#offers = offers;
     this.#placeRenderList = placeRenderList;
   }
 
-  init(point) {
+  init(point,destinations,offers) {
     this.#point = point;
     const prevPointComponent = this.#pointComponent;
     const prevEditPointComponent = this.#editPointComponent;
@@ -31,24 +35,12 @@ export default class PointPresenter {
 
     this.#renderPoint(this.#point,this.#destinations,this.#offers)
 
-  }
+    const pointDestination = destinations.find((item) => item.id === point.destination);
+    const destinationName = `${point.type} ${destination.name}`;
+    const offerListByTypePoint = offers.find((item) => item.type === point.type);
+    const currentOfferList = offerListByTypePoint.offers.filter((offer) => point.offers.find((item) => offer.id === item));
 
-  #renderPoint (currentPoint, boardDestinations, boardOffers) {
-
-    const pointDestination = boardDestinations.find((item) => item.id === currentPoint.destination);
-    const destinationName = `${currentPoint.type} ${pointDestination.name}`;
-    const offerListByTypePoint = boardOffers.find((item) => item.type === currentPoint.type);
-    const currentOfferList = offerListByTypePoint.offers.filter((offer) => currentPoint.offers.find((item) => offer.id === item));
-
-    const escKeyDownHandler = (evt) => {
-      if ((evt.key === 'Escape') || (evt.key === 'esc')) {
-        evt.preventDefault();
-        replaceEditPointToPoint();
-        document.removeEventListener('keydown',escKeyDownHandler);
-      }
-    }
-
-    this.#pointComponent = new PointView({point:currentPoint,
+    this.#pointComponent = new PointView({point:point,
       destinationName: destinationName,
       currentOfferList: currentOfferList,
       onClickButtonArrow: () => {
@@ -57,10 +49,10 @@ export default class PointPresenter {
       }
     })
 
-    const editDestinationPoint = this.#destinations.find((item) => item.id === currentPoint.destination);
+    const editDestinationPoint = this.#destinations.find((item) => item.id === point.destination);
     const editOffersByType = this.#offers.find((item) => item.type === currentPoint.type);
 
-    this.#editPointComponent = new EditPointView({point:currentPoint,
+    this.#editPointComponent = new EditPointView({point:point,
       destination:editDestinationPoint,
       offers:editOffersByType,
       onEditFormButtonSave: () => {
@@ -72,6 +64,39 @@ export default class PointPresenter {
         document.removeEventListener('keydown',escKeyDownHandler);
       }
     });
+
+    if (prevPointComponent === null || prevEditPointComponent === null) {
+      render(this.#pointComponent, this.#placeRenderList);
+      return;
+    }
+
+    if (this.#mode === Mode.DEFAULT) {
+      replace(this.#pointComponent, prevPointComponent);
+    }
+
+    if (this.#mode === Mode.EDITING) {
+      replace(this.#editPointComponent, prevPointEditComponent);
+    }
+
+    remove(prevPointComponent);
+    remove(prevEditPointComponent);
+
+  }
+
+  #renderPoint (currentPoint, boardDestinations, boardOffers) {
+
+
+    const escKeyDownHandler = (evt) => {
+      if ((evt.key === 'Escape') || (evt.key === 'esc')) {
+        evt.preventDefault();
+        replaceEditPointToPoint();
+        document.removeEventListener('keydown',escKeyDownHandler);
+      }
+    }
+
+
+
+
 
     function replacePointToEditPoint(){
       replace(editPointComponent, pointComponent);
