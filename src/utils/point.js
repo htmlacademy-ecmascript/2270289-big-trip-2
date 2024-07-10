bdba56cf2ec5ae014cabbef6899f5f2f413e0b61
AistVolhv <istanik@ya.ru>
2024-07-10 12:32:13 +0500

    Сортировка по времени.

--- src/utils/point.js
+++ src/utils/point.js
@@ -1,6 +1,25 @@
 import dayjs from 'dayjs';
 
-const getDifferensInMilliseconds = (dateFrom, dateTo) => dayjs.utc(dateTo).diff(dayjs.utc(dateFrom));
+//const getDifferensInMilliseconds = (dateFrom, dateTo) => dayjs.utc(dateTo).diff(dayjs.utc(dateFrom));
+//const getDifferensInMilliseconds = (dateFrom, dateTo) => dayjs(dateTo).diff(dayjs(dateFrom));
+
+const getDurationEventPointHour = (dateFrom, dateTo) => dayjs(dateTo).diff(dateFrom,'hour');
+
+function durationEventPoint(dueDateFrom,dueDateTo) {
+  if (dueDateFrom && dueDateTo) {
+    const differentDays = dayjs(dueDateTo).diff(dueDateFrom,'day');
+    if (differentDays < 1) {
+      const differentHour = dayjs(dueDateTo).diff(dueDateFrom,'hour');
+      return `${differentHour}H`;
+    } else {
+      const additionalDate = dayjs(dueDateFrom).add(differentDays,'day');
+      const differentHourTwo = dayjs(dueDateTo).diff(additionalDate,'hour');
+      return `${differentDays}D ${differentHourTwo}H`;
+    }
+  } else {
+    return '';
+  }
+}
 
 function sortPointDay(pointA, pointB) {
   console.log('Сортируем по дате');
@@ -9,9 +28,9 @@ function sortPointDay(pointA, pointB) {
 
 function sortPointTime(pointA, pointB) {
   console.log('Сортируем по времени')
-  const durationInHoursA = getDifferensInMilliseconds(pointA.dateFrom, pointA.dateTo);
-  const durationInHoursB = getDifferensInMilliseconds(pointB.dateFrom, pointB.dateTo);
-  return durationInHoursB - durationInHoursA;
+  const durationPointA = getDurationEventPointHour(pointA.dateFrom, pointA.dateTo);
+  const durationPointB = getDurationEventPointHour(pointB.dateFrom, pointB.dateTo);
+  return durationPointA - durationPointB;
 }
 
 function sortPointPrice(pointA, pointB) {
