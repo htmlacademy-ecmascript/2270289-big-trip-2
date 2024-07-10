bda292ca74ae41457a6b3bd850d7df535429262a
AistVolhv <istanik@ya.ru>
2024-07-10 12:36:24 +0500

    Рефакторинг utils/point.js

--- src/utils/point.js
+++ src/utils/point.js
@@ -2,21 +2,14 @@ import dayjs from 'dayjs';
 
 const getDurationEventPointHour = (dateFrom, dateTo) => dayjs(dateTo).diff(dateFrom,'hour');
 
-function sortPointDay(pointA, pointB) {
-  console.log('Сортируем по дате');
-  return dayjs(pointA.dateFrom).diff(dayjs(pointB.dateFrom))
-}
+const sortPointDay = (pointA, pointB) => dayjs(pointA.dateFrom).diff(dayjs(pointB.dateFrom));
 
-function sortPointTime(pointA, pointB) {
-  console.log('Сортируем по времени')
+const sortPointTime = (pointA, pointB) => {
   const durationPointA = getDurationEventPointHour(pointA.dateFrom, pointA.dateTo);
   const durationPointB = getDurationEventPointHour(pointB.dateFrom, pointB.dateTo);
   return durationPointA - durationPointB;
-}
+};
 
-function sortPointPrice(pointA, pointB) {
-  console.log('Сортируем по цене')
-  return pointA.basePrice - pointB.basePrice;
-}
+const sortPointPrice = (pointA, pointB) => pointA.basePrice - pointB.basePrice;
 
 export {sortPointDay,sortPointTime,sortPointPrice};
